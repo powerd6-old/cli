7097776ccd7f7f68f0dc1bd325b2a3388ae3ed77
Héctor Castelli Zacharias <hector.zacharias@gmail.com>
2022-06-24 15:33:44 +0200

    add validation when loading content

--- src/lib/module.ts
+++ src/lib/module.ts
@@ -2,9 +2,20 @@ import assert = require('assert');
 import {existsSync, mkdirSync, writeFileSync} from 'fs';
 import {Module, _Module} from '@powerd6/schemas/src/lib/schemas/module';
 import {_Author} from '@powerd6/schemas/src/lib/schemas/author';
+import {_Attribute} from '@powerd6/schemas/src/lib/schemas/attribute';
+import {_Character} from '@powerd6/schemas/src/lib/schemas/character';
+import {_Effect} from '@powerd6/schemas/src/lib/schemas/effect';
+import {_Focus} from '@powerd6/schemas/src/lib/schemas/focus';
+import {_Injury} from '@powerd6/schemas/src/lib/schemas/injury';
+import {_Item} from '@powerd6/schemas/src/lib/schemas/item';
+import {_Reference} from '@powerd6/schemas/src/lib/schemas/reference';
+import {_Rule} from '@powerd6/schemas/src/lib/schemas/rule';
+import {_Species} from '@powerd6/schemas/src/lib/schemas/species';
+import {_Spell} from '@powerd6/schemas/src/lib/schemas/spell';
 import {resolve} from 'path';
 import {Configuration} from './configuration';
 import {findDataFile, getDataFilesInFolder, loadDataFile} from './datafile';
+import {_MarkdownString} from '@powerd6/schemas/src/lib/schemas/markdown-string';
 
 export function getModuleDefinition(configuration: Configuration) {
   const modulePath = resolve(configuration.source?.directory || './');
@@ -41,7 +52,9 @@ export function withContent(
         configuration.source?.directory,
         configuration.source?.content?.get(modelKey) || modelKey
       )
-    ).map(contentFilePath => loadDataFile(contentFilePath));
+    )
+      .map(contentFilePath => loadDataFile(contentFilePath))
+      .map(contents => validateContents(contents, modelKey));
     module.content.set(modelKey, content);
   });
   return module;
@@ -67,4 +80,58 @@ export function fromFile(configuration: Configuration) {
     _Module.parse(loadDataFile(moduleFilePath));
   }
   return undefined;
+}
+
+function validateContents(contents: any, modelKey: string): any {
+  let validator;
+  switch (modelKey) {
+    case 'attribute':
+      validator = _Attribute;
+      break;
+    case 'author':
+      validator = _Author;
+      break;
+    case 'character':
+      validator = _Character;
+      break;
+    case 'effect':
+      validator = _Effect;
+      break;
+    case 'focus':
+      validator = _Focus;
+      break;
+    case 'injury':
+      validator = _Injury;
+      break;
+    case 'item':
+      validator = _Item;
+      break;
+    case 'markdown-string':
+      validator = _MarkdownString;
+      break;
+    case 'module':
+      validator = _Module;
+      break;
+    case 'reference':
+      validator = _Reference;
+      break;
+    case 'rule':
+      validator = _Rule;
+      break;
+    case 'species':
+      validator = _Species;
+      break;
+    case 'spell':
+      validator = _Spell;
+      break;
+
+    default:
+      break;
+  }
+
+  if (validator) {
+    return validator.parse(contents);
+  } else {
+    return contents;
+  }
 }
